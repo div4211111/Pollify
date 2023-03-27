71f063b9fd6997bd0fa834b58247a5ed1e6bda4f
div4211111 <andrew@Andrews-MacBook-Pro.local>
2023-03-27 14:54:08 +0300

    Creating a Repository to Work with RedisJSON

--- server/src/polls/polls.service.ts
+++ server/src/polls/polls.service.ts
@@ -1,28 +1,49 @@
+import { Logger } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
 import { createPollID, createUserID } from 'src/ids';
+import { PollsRepository } from './polls.repository';
 import { CreatePollFields, JoinPollFields, RejoinPollFiels } from './types';
 
 @Injectable()
 export class PollsService {
+  private readonly logger = new Logger(PollsService.name);
+  constructor(private readonly pollsRepository: PollsRepository) { }
   async createPoll(fields: CreatePollFields) {
     const pollID = createPollID();
     const userID = createUserID();
 
-    return {
+    const createdPoll = await this.pollsRepository.createPoll({
       ...fields,
       userID,
       pollID,
+    });
+    return {
+      poll: createdPoll,
     };
   }
   async joinPoll(fields: JoinPollFields) {
     const userID = createUserID();
 
+    this.logger.debug(
+      `Fetching poll with ID: ${fields.pollID} for user with ID: ${userID}`,
+    );
+
+    const joinedPoll = await this.pollsRepository.getPoll(fields.pollID);
+
+    // TODO - create access Token
+
     return {
-      ...fields,
-      userID,
+      poll: joinedPoll,
+      // accessToken: signedString,
     };
   }
   async rejoinPoll(fields: RejoinPollFiels) {
-    return fields;
+    this.logger.debug(
+      `Rejoining poll with ID: ${fields.pollID} for user with ID: ${fields.userID} with name: ${fields.name}`,
+    );
+
+    const joinedPoll = await this.pollsRepository.addParticipant(fields);
+
+    return joinedPoll;
   }
 }
--- server/src/polls/types.ts
+++ server/src/polls/types.ts
@@ -14,3 +14,16 @@ export type RejoinPollFiels = {
   userID: string;
   name: string;
 };
+
+export type CreatePollData = {
+  pollID: string;
+  topic: string;
+  votesPerVoter: number;
+  userID: string;
+};
+
+export type AddParticipantData = {
+  pollID: string;
+  userID: string;
+  name: string;
+};
